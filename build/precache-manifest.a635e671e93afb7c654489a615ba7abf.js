70da96b422692e8ccdf71d89e1ab6e1ef2d5a595
liushuo <liushuozaixian@163.com>
2020-04-15 14:09:59 +0800

    添加build文件

---
+++ build/precache-manifest.a635e671e93afb7c654489a615ba7abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d47ea5f370f8978d338c33d3c79ebdc",
+    "url": "/reactdemo1/index.html"
+  },
+  {
+    "revision": "90cc3e76e7295ae4173a",
+    "url": "/reactdemo1/static/css/main.52ae229f.chunk.css"
+  },
+  {
+    "revision": "d48b9e6aeda5804925f3",
+    "url": "/reactdemo1/static/js/2.45cd71b1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactdemo1/static/js/2.45cd71b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cc3e76e7295ae4173a",
+    "url": "/reactdemo1/static/js/main.2894447d.chunk.js"
+  },
+  {
+    "revision": "fd1f91bc5627c8d14368",
+    "url": "/reactdemo1/static/js/runtime-main.58833474.js"
+  }
+]);
